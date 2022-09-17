671134fbe503bb00c57e12e1b1289936aa9a4b35
Germanilu <lucianogermani93@gmail.com>
2022-09-17 17:48:35 +0200

    Feat: Create logic to check if there is a winner

--- build/main.js
+++ build/main.js
@@ -1,37 +1,5 @@
 "use strict";
-//Function to add cross
-const player = (e) => {
-    //Add cross on box
-    e.target.classList.add("cross");
-    //Get the id 
-    let idNumber = e.target.id;
-    console.log("SOY ID NUMBER ", idNumber);
-    //Transform to integer
-    let num = parseInt(idNumber);
-    // Remove the integer from the array
-    game = game.filter(e => e !== num);
-    setTimeout(() => {
-        return ia(game);
-    }, 500);
-};
-//Function IA
-const ia = (arr) => {
-    //Almaceno el indice aleatorio en la variable random
-    let random = Math.floor(Math.random() * arr.length);
-    //Pickeo algo aleatorio del array
-    let iaPick = arr[random];
-    //Convierto a string
-    let iaPickString = iaPick.toString();
-    //Quito del array lo que pikea el IA
-    varArray.forEach(e => {
-        if (iaPickString == (e === null || e === void 0 ? void 0 : e.dataset.value)) {
-            //Add Circle on box
-            e.classList.add("circle");
-            //Remove from array Game
-            game = game.filter(e => e !== iaPick);
-        }
-    });
-};
+//Setting up DOM Elements
 let first = document.getElementById('1');
 let second = document.getElementById('2');
 let third = document.getElementById('3');
@@ -41,8 +9,71 @@ let sixth = document.getElementById('6');
 let seventh = document.getElementById('7');
 let eighth = document.getElementById('8');
 let ninth = document.getElementById('9');
+//Arrays
+//Array combinaciones ganadoras
+let arrWinner = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 let game = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let varArray = [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth];
+let playerPick = [];
+let iaPick = [];
+//Variables
+let user = "USER";
+let ia = "IA";
+let winner = false;
+//Functions
+//Player Game
+const player = (e) => {
+    //Add cross on box
+    e.target.classList.add("cross");
+    //Get the id of the element
+    let idNumber = e.target.id;
+    //Transform to integer
+    let num = parseInt(idNumber);
+    // Remove the integer from the array
+    game = game.filter(e => e !== num);
+    // Pusheo lo escogido por el player al array de playerPick
+    playerPick.push(num);
+    //Lanzo funcion para revisar si hay ganador
+    checkWinner(playerPick, user);
+    //Checking if we have a winner...
+    setTimeout(() => {
+        winner ? console.log("Game end") : iaDecision(game);
+    }, 500);
+};
+//IA Game
+const iaDecision = (arr) => {
+    //Almaceno el indice aleatorio en la variable random
+    let random = Math.floor(Math.random() * arr.length);
+    //Pickeo algo aleatorio del array
+    let iaNum = arr[random];
+    // Pusheo lo escogido por el IA al array de iaPick
+    iaPick.push(iaNum);
+    //Convierto a string
+    let iaPickString = iaNum.toString();
+    //Quito del array lo que pikea el IA
+    varArray.forEach(e => {
+        if (iaPickString == (e === null || e === void 0 ? void 0 : e.dataset.value)) {
+            //Add Circle on box
+            e.classList.add("circle");
+            //Remove from array Game
+            game = game.filter(e => e !== iaNum);
+        }
+    });
+    //Checking if we have a winner...
+    checkWinner(iaPick, ia);
+    winner ? console.log("Game end") : console.log("User need to pick");
+};
+//Function to check if we have a winner...
+const checkWinner = (arrPick, winnerName) => {
+    //Map arrWinner and check if arrPick include all the number to complete 1 winner combination if yes, declare a winner
+    arrWinner.forEach(i => {
+        if (i.every(e => arrPick.includes(e))) {
+            console.log(winnerName, "WIN");
+            return winner = true;
+        }
+    });
+};
+//Event Listener
 first === null || first === void 0 ? void 0 : first.addEventListener('click', player);
 second === null || second === void 0 ? void 0 : second.addEventListener('click', player);
 third === null || third === void 0 ? void 0 : third.addEventListener('click', player);
--- main.ts
+++ main.ts
@@ -1,69 +1,95 @@
-//Function to add cross
+//Setting up DOM Elements
+let first: HTMLElement | null = document.getElementById('1')
+let second: HTMLElement | null = document.getElementById('2')
+let third: HTMLElement | null = document.getElementById('3')
+let fourth: HTMLElement | null = document.getElementById('4')
+let fifth: HTMLElement | null = document.getElementById('5')
+let sixth: HTMLElement | null = document.getElementById('6')
+let seventh: HTMLElement | null = document.getElementById('7')
+let eighth: HTMLElement | null = document.getElementById('8')
+let ninth: HTMLElement | null = document.getElementById('9')
+
+
+//Arrays
+//Array combinaciones ganadoras
+let arrWinner: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+let game: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let varArray = [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth];
+let playerPick: number[] = [];
+let iaPick: number[] = [];
+
+//Variables
+let user: string = "USER";
+let ia: string = "IA";
+let winner: boolean = false;
+
+//Functions
+
+//Player Game
 const player = (e: any) => {
     //Add cross on box
-    e.target.classList.add("cross")
-    //Get the id 
-    let idNumber: string = e.target.id
-    console.log("SOY ID NUMBER ",idNumber)
+    e.target.classList.add("cross");
+    //Get the id of the element
+    let idNumber: string = e.target.id;
     //Transform to integer
-    let num = parseInt(idNumber)
+    let num: number = parseInt(idNumber);
     // Remove the integer from the array
-    game = game.filter(e => e!== num)
+    game = game.filter(e => e !== num);
+    // Pusheo lo escogido por el player al array de playerPick
+    playerPick.push(num);
+    //Lanzo funcion para revisar si hay ganador
+    checkWinner(playerPick, user);
+    //Checking if we have a winner...
     setTimeout(() => {
-        return ia(game)
+        winner ? console.log("Game end") : iaDecision(game);
     }, 500);
-}
+};
 
 
-//Function IA
-const ia = (arr: number[]) => {
+//IA Game
+const iaDecision = (arr: number[]) => {
     //Almaceno el indice aleatorio en la variable random
     let random: number = Math.floor(Math.random() * arr.length);
     //Pickeo algo aleatorio del array
-    let iaPick: number = arr[random]
+    let iaNum: number = arr[random];
+    // Pusheo lo escogido por el IA al array de iaPick
+    iaPick.push(iaNum);
     //Convierto a string
-    let iaPickString: string = iaPick.toString()
+    let iaPickString: string = iaNum.toString();
     //Quito del array lo que pikea el IA
     varArray.forEach(e => {
-        if(iaPickString == e?.dataset.value){
+        if (iaPickString == e?.dataset.value) {
             //Add Circle on box
-            e.classList.add("circle")
+            e.classList.add("circle");
             //Remove from array Game
-            game = game.filter(e => e!== iaPick)
+            game = game.filter(e => e !== iaNum);
         }
-    })
+    });
+    //Checking if we have a winner...
+    checkWinner(iaPick, ia);
+    winner ? console.log("Game end") : console.log("User need to pick");
 }
 
 
-let first: HTMLElement|null = document.getElementById('1')
-let second: HTMLElement|null = document.getElementById('2')
-let third:HTMLElement|null = document.getElementById('3')
-let fourth:HTMLElement|null = document.getElementById('4')
-let fifth: HTMLElement|null = document.getElementById('5')
-let sixth: HTMLElement|null = document.getElementById('6')
-let seventh: HTMLElement|null = document.getElementById('7')
-let eighth: HTMLElement|null = document.getElementById('8')
-let ninth: HTMLElement|null = document.getElementById('9')
+//Function to check if we have a winner...
+const checkWinner = (arrPick: number[], winnerName: string) => {
+    //Map arrWinner and check if arrPick include all the number to complete 1 winner combination if yes, declare a winner
+    arrWinner.forEach(i => {
+        if (i.every(e => arrPick.includes(e))) {
+            console.log(winnerName, "WIN");
+            return winner = true;
+        }
+    });
+};
 
 
-let game: number[] = [1,2,3,4,5,6,7,8,9]
-let varArray = [first, second, third,fourth,fifth,sixth,seventh,eighth,ninth]
-
-
-
-first?.addEventListener('click', player)
-second?.addEventListener('click', player)
-third?.addEventListener('click', player)
-fourth?.addEventListener('click', player)
-fifth?.addEventListener('click', player)
-sixth?.addEventListener('click', player)
-seventh?.addEventListener('click', player)
-eighth?.addEventListener('click', player)
-ninth?.addEventListener('click', player)
-
-
-
-
-
-
-
+//Event Listener
+first?.addEventListener('click', player);
+second?.addEventListener('click', player);
+third?.addEventListener('click', player);
+fourth?.addEventListener('click', player);
+fifth?.addEventListener('click', player);
+sixth?.addEventListener('click', player);
+seventh?.addEventListener('click', player);
+eighth?.addEventListener('click', player);
+ninth?.addEventListener('click', player);
